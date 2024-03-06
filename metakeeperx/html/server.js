91801951253e626e5beb55678b5bde5fa8f30060
Hystrix <2669731677@qq.com>
2024-03-06 23:28:00 +0800

    new

--- metakeeperx/html/server.js
+++ metakeeperx/html/server.js
@@ -5,6 +5,7 @@ const app = express();
 app.get("/",(req,res)=>{
     res.sendFile(path.join( __dirname + "/nft.html"));
 })
-const server = app.listen(5000);
-const portNumber=server.address().port;
-console.log( `port is open on ${portNumber}` );
+const server = app.listen(5000, () => {
+    const portNumber = server.address().port;
+    console.log( `port is open on ${portNumber}` );
+});
