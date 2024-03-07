c06899d03b06fb4de83ebe35b30e566da6df6cb4
Hystrix <2669731677@qq.com>
2024-03-07 14:59:34 +0800

    yy

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static('metakeeperx'))
-app.get("/",(req,res)=>{
-    res.sendFile(path.join( __dirname + "/metakeeperx/html/home.html"));
-})
-app.get("/gamepass.html",(req,res)=>{
-    res.sendFile(path.join( __dirname + "/metakeeperx/html/gamepass.html"));
-})
-app.get("/nft.html",(req,res)=>{
-    res.sendFile(path.join( __dirname + "/metakeeperx/html/nft.html"));
-})
+// app.get("/",(req,res)=>{
+//     res.sendFile(path.join( __dirname + "/metakeeperx/html/index.html"));
+// })
+// app.get("/gamepass.html",(req,res)=>{
+//     res.sendFile(path.join( __dirname + "/metakeeperx/html/gamepass.html"));
+// })
+// app.get("/nft.html",(req,res)=>{
+//     res.sendFile(path.join( __dirname + "/metakeeperx/html/nft.html"));
+// })
 
-app.get("/home.html",(req,res)=>{
-    res.sendFile(path.join( __dirname + "/metakeeperx/html/home.html"));
-})
+// app.get("/home.html",(req,res)=>{
+//     res.sendFile(path.join( __dirname + "/metakeeperx/html/home.html"));
+// })
 const server = app.listen(8082, () => {
     const portNumber = server.address().port;
     console.log( `port is open on ${portNumber}` );
