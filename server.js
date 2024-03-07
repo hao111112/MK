8f2cdde89bc3b498d005ffbdb3c3c2a86c0fc8df
Hystrix <2669731677@qq.com>
2024-03-07 10:47:25 +0800

    0307

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+app.use(express.static('metakeeperx'))
 app.get("/",(req,res)=>{
-    res.sendFile(path.join( __dirname + "/nft.html"));
+    res.sendFile(path.join( __dirname + "/metakeeperx/html/nft.html"));
 })
 const server = app.listen(8081, () => {
     const portNumber = server.address().port;
