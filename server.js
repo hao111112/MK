780fee95d3cf89122e2302e6a73c3038245512b0
Hystrix <2669731677@qq.com>
2024-03-07 11:36:35 +0800

    new

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require("path");
 const app = express();
 app.use(express.static('metakeeperx'))
 app.get("/",(req,res)=>{
+    res.sendFile(path.join( __dirname + "/metakeeperx/html/home.html"));
+})
+app.get("/gamepass.html",(req,res)=>{
+    res.sendFile(path.join( __dirname + "/metakeeperx/html/gamepass.html"));
+})
+app.get("/nft.html",(req,res)=>{
     res.sendFile(path.join( __dirname + "/metakeeperx/html/nft.html"));
 })
 const server = app.listen(8081, () => {
